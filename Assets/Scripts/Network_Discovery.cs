4f40384b7db54dad0c865035e7070a9779f59b98
Bastien Girschig <bastien.girschig@gmail.com>
2021-03-14 17:50:35 +0100

    network discovery round trip time calculation switch to protobuf messages

--- Assets/Scripts/Network_Discovery.cs
+++ Assets/Scripts/Network_Discovery.cs
@@ -1,61 +1,35 @@
-using System.Collections;
- using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using System;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq;
+using Google.Protobuf.Network;
+using Google.Protobuf;
 
+// TODO: This is using Unity's Time.time for now, but we should probably switch to a
+// more precise time measurement later. Otherwise, we'll start losing milisecond precision
+// after a few hours running Time.time is a float
+
+class Sibling {
+    public string ip;
+    public float lastPing;
+    public double latency;
+}
 public class Network_Discovery : MonoBehaviour
 {
-    class SiblingInfo {
-        public string ip;
-        public float lastPing;
-        public float latency;
-    }
-
-    struct PingMessage {
-        public string ip;
-        public float senderTime;
-
-        public byte[] toBytes() {
-            var stringData = String.Join(",", ip, senderTime);
-            return Encoding.UTF8.GetBytes(stringData);
-        }
-        public static PingMessage fromBytes(byte[] buffer) {
-            var text = Encoding.UTF8.GetString(buffer);
-            var parts = text.Split(',');
-            return new PingMessage() {
-                ip = parts[0],
-                senderTime = float.Parse(parts[1])
-            };
-        }
-    }
-
-    struct PingResponse {
-        public string ip;
-        // Keep track of the sender's time when he sent the ping, so that it can compute the Round trip time
-        // upon receiving this response
-        public float originalSenderTime;
-
-        public byte[] toBytes() {
-            var stringData = String.Join(",", ip, originalSenderTime);
-            return Encoding.UTF8.GetBytes(stringData);
-        }
-    }
-
+    // settings
     public int port = 9876;
     public float siblingMaxStale = 5;
     public float pingInterval = 2;
+
+    // Network client
     private UdpClient udpClient;
     private IPEndPoint from;
 
+    // status
     private float lastBroadcastTime = 0;
     private string localIp = "0.0.0.0";
-    // private List<SiblingInfo> siblings = new List<SiblingInfo>();
-    private Dictionary<string, SiblingInfo> siblings = new Dictionary<string, SiblingInfo>();
+    private Dictionary<string, Sibling> siblings = new Dictionary<string, Sibling>();
     bool waitingUDP = false;
 
     void Start() {
@@ -65,7 +39,8 @@ public class Network_Discovery : MonoBehaviour
         from = new IPEndPoint(0, 0);
         localIp = getLocalIPAddress();
 
-        broadcast();
+        // Don't wait to send broadcast
+        broadcast_ping();
     }
 
     async void Update() {
@@ -73,30 +48,45 @@ public class Network_Discovery : MonoBehaviour
             waitingUDP = true; // TODO: this feels stupid. isn't there a better way ?
             var response = await udpClient.ReceiveAsync();
             waitingUDP = false;
-
-            onReceivedMessage(response);
+            onReceive(response);
         }
 
         removeStaleSiblings();
-
-        if (Time.time - lastBroadcastTime > pingInterval) {
-            broadcast();
-        }
+        if (Time.time - lastBroadcastTime > pingInterval) broadcast_ping();
     }
 
-    void onReceivedMessage(UdpReceiveResult response) {
-        var responseData = PingMessage.fromBytes(response.Buffer);
+    // "sorting station" for udp messages:
+    // Calls the appropriate handler depending on the message type
+    void onReceive(UdpReceiveResult response) {
+        var message = UdpMessage.Parser.ParseFrom(response.Buffer);
+        switch (message.MessageCase)
+        {
+            case UdpMessage.MessageOneofCase.PingMessage:
+                onReceive(message.PingMessage);
+                break;
+            default:
+                Debug.LogError("Received unexpected message format");
+                break;
+        }        
+    }
 
-        bool siblingsChanged = false;
-        if (!siblings.ContainsKey(responseData.ip)) {
-            siblings[responseData.ip] = new SiblingInfo();
-            siblingsChanged = true;
+    void onReceive(PingMessage ping) {
+        if (ping.ToIp == "all") {
+            sendPingResponse(ping);
+        } else {
+            double roundTripTime = Time.time - ping.SentTime;
+
+            bool siblingsChanged = false;
+            if (!siblings.ContainsKey(ping.ToIp)) {
+                siblings[ping.ToIp] = new Sibling();
+                siblingsChanged = true;
+            }
+            siblings[ping.ToIp].ip = ping.ToIp;
+            siblings[ping.ToIp].lastPing = Time.time;
+            siblings[ping.ToIp].latency = roundTripTime / 2.0f;
+
+            if (siblingsChanged) onSiblingsChanged();
         }
-        siblings[responseData.ip].ip = responseData.ip;
-        siblings[responseData.ip].lastPing = Time.time;
-        siblings[responseData.ip].latency = responseData.senderTime - Time.time;
-
-        if (siblingsChanged) onSiblingsChanged();
     }
 
     void removeStaleSiblings() {
@@ -113,16 +103,28 @@ public class Network_Discovery : MonoBehaviour
         }
     }
 
-    void broadcast() {
+    void broadcast_ping() {
         lastBroadcastTime = Time.time;
-        var message = new PingMessage() { ip = localIp, senderTime = Time.time };
-        var data = message.toBytes();
+
+        var data = new UdpMessage() {
+            PingMessage = new PingMessage() {
+                FromIp = localIp,
+                ToIp = "all",
+                SentTime = Time.time,
+            }
+        }.ToByteArray();
         udpClient.Send(data, data.Length, "255.255.255.255", port);
     }
+
     void sendPingResponse(PingMessage ping) {
-        var message = new PingResponse() { ip = localIp, originalSenderTime = ping.senderTime };
-        var data = message.toBytes();
-        udpClient.Send(data, data.Length, ping.ip, port);
+        var data = new UdpMessage() {
+            PingMessage = new PingMessage() {
+                FromIp = ping.FromIp,
+                ToIp = localIp,
+                SentTime = ping.SentTime,
+            }
+        }.ToByteArray();
+        udpClient.Send(data, data.Length, ping.FromIp, port);
     }
 
     public static string getLocalIPAddress() {
@@ -144,9 +146,9 @@ public class Network_Discovery : MonoBehaviour
     {
         string infoString = "";
         infoString += String.Format("local IP:\n  {0}\n", localIp);
-        infoString += "siblings (IP, last ping):\n";
+        infoString += "siblings (IP, last ping, latency):\n";
         foreach (var sibling in siblings.Values) {
-            infoString += String.Format("  - {0} {1}s\n", sibling.ip, Time.time - sibling.lastPing);
+            infoString += String.Format("  - {0}\t{1:0.00}s\t{2:0.00}ms\n", sibling.ip, Time.time - sibling.lastPing, sibling.latency*1000);
         }
         GUI.TextArea(new Rect(0,0,300,100), infoString);
     }
