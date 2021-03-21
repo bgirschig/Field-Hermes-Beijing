6c8aa0e614dffaec5ef0e229c0bf3e8b14293318
Bastien Girschig <bastien.girschig@gmail.com>
2021-03-21 19:37:52 +0100

    Child swapper script
    Allows easily comparing different versions of an object

---
+++ Assets/Scripts/ChildSwapper.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Swap between the child objects. Useful to compare them
+
+public class ChildSwapper : MonoBehaviour
+{
+    int currentIdx = -1;
+    GameObject[] children;
+    public string key = "space";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        children = new GameObject[transform.childCount];
+        int idx = 0;
+        foreach (Transform child in transform) {
+            children[idx] = child.gameObject;
+            idx += 1;
+        }
+
+        nextObject();
+    }
+
+    void nextObject() {
+        foreach (GameObject child in children) {
+            child.SetActive(false);
+        }
+        currentIdx = (currentIdx+1) % transform.childCount;
+        children[currentIdx].gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key)) nextObject();
+    }
+}
