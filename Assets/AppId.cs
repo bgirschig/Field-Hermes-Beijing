f4596641eeabe9f0d685b734e05f512dec84c3ed
Bastien Girschig <bastien.girschig@gmail.com>
2021-04-26 15:11:09 +0200

    Show app identifier in UI

---
+++ Assets/AppId.cs
@@ -0,0 +1,13 @@
+/// Display the current app's ID (name, version, etc...) on the attached Text element
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppId : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Text>().text = $"{Application.identifier} {Application.version} (unity {Application.unityVersion})";
+        print($"Application.identifier {Application.identifier}");
+    }
+ }
